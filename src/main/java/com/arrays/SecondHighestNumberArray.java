490a4832eede2ee8c45c9a14eddac4c51e276146
mukesh <java@2022>
2022-07-02 19:47:08 +0530

    Find Second Highest Number in an Array

---
+++ src/main/java/com/arrays/SecondHighestNumberArray.java
@@ -0,0 +1,24 @@
+package com.arrays;
+
+import java.util.Arrays;
+
+public class SecondHighestNumberArray {
+
+	public static void main(String[] args) {
+		
+		int[] numArr = {34,56,8,24,69,19,2,5,12,65};
+		
+		int temp = 0;
+		for(int i=0; i<numArr.length; i++) {
+			for(int j=i+1; j<numArr.length; j++) {
+				if(numArr[i] < numArr[j]) {
+					temp = numArr[i];
+					numArr[i] = numArr[j];
+					numArr[j] = temp;
+				}
+			}
+		}
+		System.out.println(Arrays.toString(numArr));
+		System.out.println("Second Highest Value: "+numArr[1]);
+	}
+}
